632cc35fceaf29c6a0ce13f4c736162cb445a9c5
jflute <dbflute@gmail.com>
2014-08-23 15:27:23 +0900

    (DBFLUTE-1144){Java/C#}: 未定義の区分値を拾ってしまったときの未来オプション

--- dbflute-sastruts-example/src/main/java/com/example/dbflute/sastruts/dbflute/bsentity/dbmeta/MemberLoginDbm.java
+++ dbflute-sastruts-example/src/main/java/com/example/dbflute/sastruts/dbflute/bsentity/dbmeta/MemberLoginDbm.java
@@ -76,7 +76,12 @@ public class MemberLoginDbm extends AbstractDBMeta {
         public void write(Entity et, Object vl) {
             ColumnInfo col = columnMobileLoginFlg();
             ccls(col, vl);
-            ((MemberLogin)et).setMobileLoginFlgAsFlg((CDef.Flg)gcls(col, vl));
+            CDef.Flg cls = (CDef.Flg)gcls(col, vl);
+            if (cls != null) {
+                ((MemberLogin)et).setMobileLoginFlgAsFlg(cls);
+            } else {
+                ((MemberLogin)et).mynativeMappingMobileLoginFlg(ctn(vl, Integer.class));
+            }
         }
     }
     public class EpgLoginMemberStatusCode implements PropertyGateway {
@@ -84,7 +89,12 @@ public class MemberLoginDbm extends AbstractDBMeta {
         public void write(Entity et, Object vl) {
             ColumnInfo col = columnLoginMemberStatusCode();
             ccls(col, vl);
-            ((MemberLogin)et).setLoginMemberStatusCodeAsMemberStatus((CDef.MemberStatus)gcls(col, vl));
+            CDef.MemberStatus cls = (CDef.MemberStatus)gcls(col, vl);
+            if (cls != null) {
+                ((MemberLogin)et).setLoginMemberStatusCodeAsMemberStatus(cls);
+            } else {
+                ((MemberLogin)et).mynativeMappingLoginMemberStatusCode((String)vl);
+            }
         }
     }
     public PropertyGateway findPropertyGateway(String prop)
